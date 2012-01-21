f8fb4fef3a49d545da7323f0c66330d0225d5785
Julien Quint <julien@igel.co.jp>
2012-01-21 19:41:43 +0900

    Upgrading animatic

---
+++ off/animatic/animatic.js
@@ -0,0 +1,20 @@
+exports.PATTERNS =
+[
+
+  ["GET", /^\/animatic\/images\/(.+)$/, function(transaction, img) {
+      transaction.serve_file_from_path(req, response, path.join("images", img));
+    }],
+
+  ["POST", /^\/animatic\/save\/(.+)$/, save],
+
+];
+
+function save(transaction, filename)
+{
+  var out = fs.createWriteStream(path.join("images", filename));
+  req.on("data", function(chunk) { out.write(chunk); });
+  req.on("end", function() {
+      out.end();
+      transaction.serve_json({ OK: filename });
+    });
+}
