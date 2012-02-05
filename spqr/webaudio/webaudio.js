358aabf9e1bad771b7ce2623e027630cfc63b83e
Julien Quint <julien@igel.co.jp>
2012-02-05 19:36:48 +0900

    Webaudio tests

---
+++ spqr/webaudio/webaudio.js
@@ -0,0 +1,40 @@
+(function(webaudio) {
+
+  webaudio.context = new webkitAudioContext();
+  var BUFFER_SIZE = 512;
+
+  webaudio.create_phasor = function(freq, buffer_size)
+  {
+    var p = webaudio.context.createJavaScriptNode(buffer_size || BUFFER_SIZE,
+      0, 1);
+    p.gain = webaudio.context.createGainNode();
+    p.freq = freq || 1;
+    p.phase = 0;
+    p.onaudioprocess = function(e)
+    {
+      var out = e.outputBuffer.getChannelData(0);
+      var cycle = e.outputBuffer.sampleRate / this.freq;
+      for (var i = 0, n = this.bufferSize; i < n; ++i) {
+        out[i] = this.gain.gain.value * ((this.phase + i / cycle) % 1);
+      }
+      this.phase = this.gain.gain.value * ((this.phase + i / cycle) % 1);
+    };
+    return p;
+  }
+
+  webaudio.create_sin = function(buffer_size)
+  {
+    var s = webaudio.context.createJavaScriptNode(buffer_size || BUFFER_SIZE,
+        0, 1);
+    s.onaudioprocess = function(e)
+    {
+      var in_ = e.inputBuffer.getChannelData(0);
+      var out = e.outputBuffer.getChannelData(0);
+      for (var i = 0, n = this.bufferSize; i < n; ++i) {
+        out[i] = Math.sin(in_[i] * 2 * Math.PI);
+      }
+    };
+    return s;
+  };
+
+})(this.webaudio = {});
